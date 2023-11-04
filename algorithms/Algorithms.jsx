025908eb467c090f9a36754363e000a6ca15d860
=Dylan Kral <=dylankral@gmail.com>
2023-11-03 20:46:41 -0700

    implemented bubble sort

--- algorithms/Algorithms.jsx
+++ algorithms/Algorithms.jsx
@@ -1,6 +1,39 @@
 import { useArrayData } from "../context/ArrayDataContext";
 
 
+export async function bubbleSort(arr, setData){
+  console.log("Bubble Sort Starting")
+
+  const sortedArray = [...arr]
+
+  for( let step = 0; step < sortedArray.length -1; step++){
+    for(let j = 0; j < sortedArray.length - step -1; j++){
+      sortedArray[j].isExamined = true
+      sortedArray[j+1].isExamined = true
+
+
+      await new Promise((resolve) => setTimeout(resolve, 50));
+      setData([...sortedArray])
+
+      let temp = sortedArray[j]
+      if(sortedArray[j+1].value < sortedArray[j].value){
+        sortedArray[j] = sortedArray[j+1]
+        sortedArray[j+1] = temp
+        sortedArray[j+1].isExamined = true
+
+      }
+
+      sortedArray[j].isExamined = false;
+      
+      await new Promise((resolve) => setTimeout(resolve,50))
+      setData([...sortedArray])
+     
+    } 
+  }
+  return sortedArray
+}
+
+
 export function binarySort(arr) {
     
 }
